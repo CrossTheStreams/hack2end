a5f2a7f4dc1ccbd9202d577101d87cc15f4f8d21
Andrew <arhautau@gmail.com>
2013-09-29 15:53:42 -0700

    getting this party started

--- app.rb
+++ app.rb
@@ -2,7 +2,19 @@
 require 'rubygems'
 require 'haml'
 require 'sinatra'
+require 'date'
 
 get '/' do
   haml :index, :format => :html5
 end
+
+get '/about' do 
+
+end
+
+get '/contact' do 
+
+end
+
+
+
